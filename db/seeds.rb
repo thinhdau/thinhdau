fe2a109eb1536f5233765463ab8f2a2ad20672dd
Thinh <bestyasuovn710@gmail.com>
2025-01-11 08:08:24 +0700

    thempass

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create!(
+  email_address: "dautrongthinh@gmail.com",
+  password: "123",
+  password_confirmation: "123"
+)
